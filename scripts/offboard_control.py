6443cb5e4273139f388ce44e2d07d124124b7b16
kikutabryan <kikutabryan@gmail.com>
2023-08-03 22:37:20 -0400

    added turtle chasing abilities

---
+++ scripts/offboard_control.py
@@ -0,0 +1,447 @@
+#!/usr/bin/python3
+import rospy
+import math
+from sensor_msgs.msg import NavSatFix
+from mavros_msgs.msg import Altitude, State, Waypoint, WaypointList, WaypointReached, PositionTarget, ParamValue
+from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest, WaypointPush, WaypointPushRequest, WaypointPull, WaypointClear, ParamSet
+from geometry_msgs.msg import PoseStamped, Point, Vector3
+
+class OffboardController:
+    def __init__(self):
+        self.current_state = State()
+        rospy.init_node('offb_node_py')
+
+        # Subscribe to necessary topics
+        self.state_sub = rospy.Subscriber('/mavros/state', State, self.state_callback)
+        self.coordinate_sub = rospy.Subscriber('/mavros/global_position/global', NavSatFix, self.gps_callback)
+        self.altitude_sub = rospy.Subscriber('/mavros/altitude', Altitude, self.altitude_callback)
+        self.waypoint_sub = rospy.Subscriber('/mavros/mission/waypoints', WaypointList, self.waypoint_list_callback)
+        self.waypoint_reached_sub = rospy.Subscriber('/mavros/mission/reached', WaypointReached, self.waypoint_reached_callback)
+        self.local_sub = rospy.Subscriber('mavros/local_position/pose', PoseStamped, self.pose_callback)
+        self.turtle_sub = rospy.Subscriber('position_turtle_bot', PoseStamped, self.update_turtle_position)
+
+        # Publish to necessary topics
+        self.position_target_pub = rospy.Publisher('/mavros/setpoint_raw/local', PositionTarget, queue_size=10)
+
+        # Establish message objects
+        self.waypoint_list = WaypointList()
+        self.reached_waypoint = WaypointReached()
+        self.current_pose = PoseStamped()
+        self.current_global = NavSatFix()
+        self.current_altitude = Altitude()
+
+        self.turtle_position = PoseStamped()
+        self.turtle_position.pose.position.x = 0
+        self.turtle_position.pose.position.y = 0
+        self.turtle_position.pose.position.z = 1.5
+
+    def state_callback(self, msg):
+        """Update the vehicle state.
+
+        Args:
+            msg (State): The new state message.
+        """
+        self.current_state = msg
+
+    def gps_callback(self, msg):
+        """Update the vehicle global position.
+
+        Args:
+            msg (NavSatFix): The new GPS message.
+        """
+        self.current_global = msg
+
+    def altitude_callback(self, msg):
+        """Update the vehicle altitude.
+
+        Args:
+            msg (Altitude): The new altitude message.
+        """
+        self.current_altitude = msg
+
+    def waypoint_list_callback(self, msg):
+        """Update the waypoint list.
+
+        Args:
+            msg (WaypointList): The new waypoint list message.
+        """
+        self.waypoint_list = msg
+
+    def waypoint_reached_callback(self, msg):
+        """Update the reached waypoint.
+
+        Args:
+            msg (WaypointReached): The new reached waypoint message.
+        """
+        self.reached_waypoint = msg
+
+    def pose_callback(self, msg):
+        """Update the current local pose.
+
+        Args:
+            msg (PoseStamped): The new pose message.
+        """
+        self.current_pose = msg
+
+    def update_turtle_position(self, msg):
+        self.turtle_position = msg
+
+    def set_parameter(self, parameter, value):
+        rospy.wait_for_service('/mavros/param/set')
+        try:
+            # Request service and get response
+            param_set_srv = rospy.ServiceProxy('/mavros/param/set', ParamSet)
+            response = param_set_srv(param_id=parameter, value=value)
+
+            # Check if service request was successful
+            if response.success:
+                rospy.loginfo('Parameter: {} set to: {}'.format(parameter, value))
+                return True
+            else:
+                rospy.logwarn('Failed to set parameter {}'.format(parameter))
+                return False
+        except rospy.ServiceException as e:
+            rospy.logerr('Service call failed: {}'.format(e))
+            return False
+
+    def set_arm(self, arm):
+        """Set the arm status of the vehicle.
+
+        Args:
+            arm (bool): True to arm the vehicle, False to disarm.
+
+        Returns:
+            bool: True if the operation is successful, False otherwise.
+        """
+        rospy.wait_for_service('/mavros/cmd/arming')
+        try:
+            # Request service and get response
+            arm_srv = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
+            arm_cmd = CommandBoolRequest()
+            arm_cmd.value = arm
+            response = arm_srv(arm_cmd)
+
+            # Check if service request was successful and drone is armed
+            if response.success and self.current_state.armed == arm:
+                rospy.loginfo('Vehicle armed: {}'.format(arm))
+                return True
+            else:
+                rospy.logwarn('Failed to arm vehicle')
+                return False
+        except rospy.ServiceException as e:
+            rospy.logerr('Service call failed: {}'.format(e))
+            return False
+
+    def set_mode(self, mode):
+        """Set the flight mode of the vehicle.
+
+        Args:
+            mode (str): The desired flight mode.
+
+        Returns:
+            bool: True if the operation is successful, False otherwise.
+        """
+        rospy.wait_for_service('/mavros/set_mode')
+        try:
+            # Request service and get response
+            set_mode_srv = rospy.ServiceProxy('/mavros/set_mode', SetMode)
+            offb_set_mode = SetModeRequest()
+            offb_set_mode.custom_mode = mode
+            response = set_mode_srv(offb_set_mode)
+
+            # Check if service request was successful and drone mode is set
+            if response.mode_sent and self.current_state.mode == mode:
+                rospy.loginfo('Mode set to: {}'.format(mode))
+                return True
+            else:
+                rospy.logwarn('Failed to set mode: {}'.format(mode))
+                return False
+        except rospy.ServiceException as e:
+            rospy.logerr('Service call failed: {}'.format(e))
+            return False
+        
+    def create_waypoint(self, frame, command, is_current, p1, p2, p3, p4, x, y, z):
+        """Create a new waypoint object.
+
+        Args:
+            frame (int): The coordinate frame of the waypoint.
+            command (int): The command associated with the waypoint.
+            is_current (bool): True if the waypoint is the current one, False otherwise.
+            p1 (float): The first parameter of the waypoint.
+            p2 (float): The second parameter of the waypoint.
+            p3 (float): The third parameter of the waypoint.
+            p4 (float): The fourth parameter of the waypoint.
+            x (float): The latitude or x-coordinate of the waypoint.
+            y (float): The longitude or y-coordinate of the waypoint.
+            z (float): The altitude or z-coordinate of the waypoint.
+
+        Returns:
+            Waypoint: The created waypoint object.
+        """
+        waypoint = Waypoint()
+        waypoint.frame = frame
+        waypoint.command = command
+        waypoint.is_current = is_current
+        waypoint.autocontinue = True
+        waypoint.param1 = float(p1)
+        waypoint.param2 = float(p2)
+        waypoint.param3 = float(p3)
+        waypoint.param4 = float(p4)
+        waypoint.x_lat = x
+        waypoint.y_long = y
+        waypoint.z_alt = z
+        return waypoint
+
+    def compare_waypoints(self, waypoint1, waypoint2):
+        """Compare two waypoints for equality.
+
+        Args:
+            waypoint1 (Waypoint): The first waypoint.
+            waypoint2 (Waypoint): The second waypoint.
+
+        Returns:
+            bool: True if the waypoints are equal, False otherwise.
+        """
+        if waypoint1.frame != waypoint2.frame:
+            return False
+        if waypoint1.command != waypoint2.command:
+            return False
+        if waypoint1.is_current != waypoint2.is_current:
+            return False
+        if waypoint1.autocontinue != waypoint2.autocontinue:
+            return False
+        if waypoint1.param1 != waypoint2.param1 and math.isnan(waypoint1.param4) != math.isnan(waypoint2.param4):
+            return False
+        if waypoint1.param2 != waypoint2.param2 and math.isnan(waypoint1.param4) != math.isnan(waypoint2.param4):
+            return False
+        if waypoint1.param3 != waypoint2.param3 and math.isnan(waypoint1.param4) != math.isnan(waypoint2.param4):
+            return False
+        if waypoint1.param4 != waypoint2.param4 and math.isnan(waypoint1.param4) != math.isnan(waypoint2.param4):
+            return False
+        if waypoint1.x_lat != waypoint2.x_lat:
+            return False
+        if waypoint1.y_long != waypoint2.y_long:
+            return False
+        if waypoint1.z_alt != waypoint2.z_alt:
+            return False
+        return True
+
+    def compare_waypoint_lists(self, waypoints1, waypoints2):
+        """Compare two lists of waypoints for equality.
+
+        Args:
+            waypoints1 (List[Waypoint]): The first list of waypoints.
+            waypoints2 (List[Waypoint]): The second list of waypoints.
+
+        Returns:
+            bool: True if the lists are equal, False otherwise.
+        """
+        # Check if the lists have the same length
+        if len(waypoints1) != len(waypoints2) or not waypoints1 or not waypoints2:
+            return False
+        
+        # Iterate over each waypoint in the lists
+        for i in range(len(waypoints1)):
+            waypoint1 = waypoints1[i]
+            waypoint2 = waypoints2[i]
+
+            # Compare the waypoints using the compare_waypoints() function
+            if not self.compare_waypoints(waypoint1, waypoint2):
+                return False
+
+        # If all waypoints are equal, the lists are equal
+        return True
+    
+    def clear_waypoints(self):
+        """Clear the current list of waypoints.
+
+        Returns:
+            bool: True if the operation is successful, False otherwise.
+        """
+        rospy.wait_for_service('/mavros/mission/clear')
+        try:
+            # Request service and get response
+            waypoint_clear_srv = rospy.ServiceProxy('/mavros/mission/clear', WaypointClear)
+            response = waypoint_clear_srv()
+
+            # Check if service request was successful and waypoints list is empty
+            if response.success and not self.waypoint_list.waypoints:
+                rospy.loginfo('Waypoint clear successful')
+                return True
+            else:
+                rospy.logwarn('Waypoint clear failed')
+                return False
+        except rospy.ServiceException as e:
+            rospy.logerr('Service call failed: {}'.format(e))
+            return False
+        
+    def send_waypoints(self, waypoints):
+        """Send a list of waypoints to the vehicle.
+
+        Args:
+            waypoints (List[Waypoint]): The waypoints to be sent.
+
+        Returns:
+            bool: True if the operation is successful, False otherwise.
+        """
+        rospy.wait_for_service('/mavros/mission/push')
+        try:
+            # Request service and get response
+            waypoint_push_srv = rospy.ServiceProxy('/mavros/mission/push', WaypointPush)
+            waypoint_push_req = WaypointPushRequest()
+            waypoint_push_req.waypoints = waypoints
+            response = waypoint_push_srv(waypoint_push_req)
+
+            # Check if service request was successful and waypoint lists match
+            if response.success and self.compare_waypoint_lists(self.waypoint_list.waypoints, waypoints):
+                rospy.loginfo('Waypoint push successful')
+                return True
+            else:
+                rospy.logwarn('Waypoint push failed')
+                return False
+        except rospy.ServiceException as e:
+            rospy.logerr('Service call failed: {}'.format(e))
+            return False
+                
+    def is_mission_completed(self):
+        """Check if the mission has been completed.
+
+        Returns:
+            bool: True if the mission is completed, False otherwise.
+        """
+        rospy.wait_for_service('/mavros/mission/pull')
+        try:
+            # Request service and get response
+            waypoint_pull_srv = rospy.ServiceProxy('/mavros/mission/pull', WaypointPull)
+            response = waypoint_pull_srv()
+
+            # Check if service request was successful
+            if response.success:
+                # Get the total waypoint sequence count
+                num_waypoints = response.wp_received - 1
+                # Check if all waypoints have been completed and reached waypoint time stamp is current
+                if self.reached_waypoint.wp_seq == num_waypoints and self.reached_waypoint.header.stamp.secs == self.current_state.header.stamp.secs:
+                    return True
+                else:
+                    return False
+            else:
+                rospy.logwarn('Failed to pull waypoints')
+                return False
+        except rospy.ServiceException as e:
+            rospy.logerr('Service call failed: {}'.format(e))
+            return False
+    
+    def target_reached(self, current, target, acceptance):
+        """Check if the target has been reached.
+
+        Args:
+            current (Pose): The current position.
+            target (Pose): The target position.
+            acceptance (float): The acceptable distance to consider the target reached.
+
+        Returns:
+            bool: True if the target is reached, False otherwise.
+        """
+        x_1, y_1, z_1 = current.x, current.y, current.z
+        x_2, y_2, z_2 = target.x, target.y, target.z
+
+        distance = math.sqrt((x_1 - x_2)**2 + (y_1 - y_2)**2 + (z_1 - z_2)**2)
+        if distance <= acceptance:
+            return True
+        else:
+            return False
+        
+    def send_position(self, position):
+        """Send the desired position to the vehicle.
+
+        Args:
+            position (geometry_msgs.msg.Point): The desired position as a 3D point (x, y, z).
+        """
+        position_msg = PositionTarget()
+        position_msg.header.stamp = rospy.Time.now()
+        position_msg.coordinate_frame = PositionTarget.FRAME_LOCAL_NED
+        position_msg.type_mask = PositionTarget.IGNORE_VX | PositionTarget.IGNORE_VY | PositionTarget.IGNORE_VZ | \
+                                PositionTarget.IGNORE_AFX | PositionTarget.IGNORE_AFY | PositionTarget.IGNORE_AFZ | \
+                                PositionTarget.IGNORE_YAW | PositionTarget.IGNORE_YAW_RATE
+        position_msg.position = position
+        self.position_target_pub.publish(position_msg)
+
+    def go_to_position(self, x, y, z):
+        """Move the vehicle to the specified position.
+
+        Args:
+            x (float): The x-coordinate of the target position.
+            y (float): The y-coordinate of the target position.
+            z (float): The z-coordinate of the target position.
+        """
+        position = Point()
+        position.x = x
+        position.y = y
+        position.z = z
+        rate = rospy.Rate(20)
+        while not rospy.is_shutdown() and not self.target_reached(self.current_pose.pose.position, position, 0.25):
+            self.send_position(position)
+            if self.current_state.mode != 'OFFBOARD':
+                self.set_mode('OFFBOARD')
+            rate.sleep()
+            
+    def send_velocity(self, velocity):
+        """Send the desired velocity to the vehicle.
+
+        Args:
+            velocity (geometry_msgs.msg.Vector3): The desired velocity as a 3D vector (vx, vy, vz).
+        """
+        velocity_msg = PositionTarget()
+        velocity_msg.header.stamp = rospy.Time.now()
+        velocity_msg.coordiante_frame = PositionTarget.FRAME_LOCAL_NED
+        velocity_msg.type_mask = PositionTarget.IGNORE_PX | PositionTarget.IGNORE_PY | PositionTarget.IGNORE_PZ | \
+                                PositionTarget.IGNORE_AFX | PositionTarget.IGNORE_AFY | PositionTarget.IGNORE_AFZ | \
+                                PositionTarget.IGNORE_YAW | PositionTarget.IGNORE_YAW_RATE
+        velocity_msg.velocity = velocity
+        self.position_target_pub.publish(velocity_msg)
+
+    def magical_shape(self):
+        start_x = self.current_pose.pose.position.x
+        start_y = self.current_pose.pose.position.y
+        start_z = self.current_pose.pose.position.z
+        scale = 10
+        point_list = [[-2, 0, 0],
+                      [-1, 0, 0],
+                      [-1, -3, 0],
+                      [0, -3, 0],
+                      [0, 0, 0],
+                      [1, -3, 0],
+                      [2, 0, 0],
+                      [2, -3, 0],
+                      [3, 0, 0],
+                      [3.5, -1.5, 0],
+                      [2.5, -1.5, 0],
+                      [3.5, -1.5, 0],
+                      [4, -3, 0],
+                      [5, -3, 0],
+                      [4, 0, 0],
+                      [5, -3, 0],
+                      [6, 0, 0]]
+        for point in point_list:
+            self.go_to_position(start_x + scale*point[0], start_y + scale*point[1], start_z + scale*point[2])
+
+    def turtle_chaser(self):
+        rate = rospy.Rate(20)
+        while not rospy.is_shutdown():
+            self.send_position(self.turtle_position.pose.position)
+            if self.current_state.mode != 'OFFBOARD':
+                self.set_mode('OFFBOARD')
+            rate.sleep()
+
+
+
+if __name__ == "__main__":
+    rospy.init_node('offb_node_py')
+    drone = OffboardController()
+    
+    rospy.loginfo('Waiting for FCU connection...')
+    while not rospy.is_shutdown() and not drone.current_state.connected:
+        rospy.sleep(0.1)
+    rospy.loginfo('FCU connected')
+
+    drone.turtle_chaser()
---
+++ scripts/turtle_position_publisher.py
@@ -0,0 +1,241 @@
+import cv2
+import numpy as np
+import ast
+import rospy
+from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+class PositionPublisher:
+    def __init__(self):
+        rospy.init_node('position_publisher')
+
+        self.frame = None
+
+        self.turtle_position = np.empty((3, 1))
+        self.turtle_position[0] = 0
+        self.turtle_position[1] = 0
+        self.turtle_position[2] = 0
+
+        self.local_pose = PoseStamped()
+
+        # Declare parameters
+        self.marker_size = rospy.get_param('marker_size', 0.075)
+        self.marker_spacing = rospy.get_param('marker_spacing', 0.522)
+        self.markers_x = rospy.get_param('markers_x', 7)
+        self.markers_y = rospy.get_param('markers_y', 7)
+
+        # Parse the camera matrix from the parameter server and convert it to a NumPy array
+        camera_matrix_str = rospy.get_param('camera_matrix', '[[1204.4697584542726, 0.0, 354.6182298916754], [0.0, 1201.8571173383637, 652.7026112972777], [0.0, 0.0, 1.0]]')
+        self.camera_matrix = np.array(ast.literal_eval(camera_matrix_str))
+
+        # Parse the distortion coefficients from the parameter server and convert them to a NumPy array
+        dist_coeffs_str = rospy.get_param('dist_coeffs', '[[0.008252427738841408, 0.17962144633096283, -0.0010671121861827175, -0.0017195153485555525, -0.7083074964169391]]')
+        self.dist_coeffs = np.array(ast.literal_eval(dist_coeffs_str))
+
+        # Create a publisher to publish positions on the specified topic
+        self.publisher1_ = rospy.Publisher('position_turtle_bot', PoseStamped, queue_size=10)
+        self.timer = rospy.Timer(rospy.Duration(1.0 / 50), self.publish_position)
+
+        # Create a subscriber to listen to the camera image topic
+        self.subscription1 = rospy.Subscriber('camera_image_topic', Image, self.update_position, queue_size=10)
+
+        # Create a subscriber to listen to the drone local position
+        self.subscription2 = rospy.Subscriber('/mavros/local_position/pose', PoseStamped, self.update_local_pose, queue_size=10)
+
+        # Create a CvBridge object to convert ROS Image messages to OpenCV images
+        self.bridge = CvBridge()
+
+        # Define the aruco dictionary and parameters
+        self.aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_1000)
+        self.aruco_params = cv2.aruco.DetectorParameters_create()
+
+        # Create the aruco board object with 4x4 grid and 16 markers
+        self.board = cv2.aruco.GridBoard_create(self.markers_y, self.markers_x, self.marker_size, self.marker_spacing, self.aruco_dict)
+
+    def update_local_pose(self, msg):
+        self.local_pose = msg
+
+    def update_position(self, msg):
+        # Convert the ROS Image message to an OpenCV image
+        self.frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+        try:
+            # Detect the aruco markers in the frame
+            corners, ids, rejected = cv2.aruco.detectMarkers(self.frame, self.aruco_dict, parameters=self.aruco_params)
+
+            # If at least one marker is detected
+            if ids is not None:
+                # Estimate the pose of the board relative to the camera
+                ret, rvec, tvec = cv2.aruco.estimatePoseBoard(corners, ids, self.board, self.camera_matrix, self.dist_coeffs, None, None, False)
+
+                # If the pose estimation is successful
+                if ret > 0:
+                    # Position of the turtlebot with respect to the drone (ENU)
+                    position_t_d = np.empty((3, 1))
+                    position_t_d[0] = tvec[0, 0]
+                    position_t_d[1] = -tvec[0, 0]
+                    position_t_d[2] = -tvec[0, 0]
+
+                    # Rotation matrix of the drone with respect to the world (FLU)
+                    quaternion_d_w = np.matrix((4, 1))
+                    quaternion_d_w[0] = self.local_pose.pose.orientation.x
+                    quaternion_d_w[1] = self.local_pose.pose.orientation.y
+                    quaternion_d_w[2] = self.local_pose.pose.orientation.z
+                    quaternion_d_w[3] = self.local_pose.pose.orientation.w
+                    rotation_d_w = self.quaternion_to_rotation_matrix(quaternion_d_w)
+
+                    # Rotate matrix to go from FLU to RFU
+                    rotation_d_w = np.matmul(rotation_d_w, self.rotation_matrix_xyz(0, 0, np.pi / 2))
+
+                    # Rotation matrix of the world with respect to the drone
+                    rotation_w_d = np.matrix(rotation_d_w).T
+
+                    # Position of the drone with respect to the world (ENU)
+                    position_d_w = np.matrix((3, 1))
+                    position_d_w[0] = self.local_pose.pose.position.x
+                    position_d_w[1] = self.local_pose.pose.position.y
+                    position_d_w[2] = self.local_pose.pose.position.z
+
+                    # Position of the turtlebot with respect to the world (ENU)
+                    position_t_w = np.matmul(rotation_w_d, position_t_d)
+                    position_t_w = position_t_w + position_d_w
+
+                    # Set the turtlebot position
+                    self.turtle_position = position_t_w
+
+        except Exception as e:
+            rospy.logerr('Error: ' + str(e))
+
+    def publish_position(self, event):
+        """
+        Publish the position and orientation as a ROS PoseStamped message.
+
+        This function is called periodically based on the timer to publish the
+        position and orientation of the ArUco board as a ROS PoseStamped message.
+
+        Parameters:
+        - event: The ROS Timer event triggering the function.
+        """
+        try:
+            # Publish the position as a ROS message
+            vision_msg = PoseStamped()
+            vision_msg.header.stamp = rospy.Time.now()
+            vision_msg.header.frame_id = 'map'
+            vision_msg.pose.position.x = float(self.turtle_position[0])
+            vision_msg.pose.position.y = float(self.turtle_position[1])
+            vision_msg.pose.position.z = float(self.turtle_position[2])
+            self.publisher1_.publish(vision_msg)
+
+        except Exception as e:
+            rospy.logerr('Error: ' + str(e))
+
+    def rotation_matrix_xyz(self, theta_x, theta_y, theta_z):
+        """
+        Create a 3x3 rotation matrix for rotations around the X, Y, and Z axes.
+
+        Parameters:
+        - theta_x: Rotation angle around the X axis in radians.
+        - theta_y: Rotation angle around the Y axis in radians.
+        - theta_z: Rotation angle around the Z axis in radians.
+
+        Returns:
+        - R: 3x3 NumPy array representing the rotation matrix.
+        """
+        cos_x = np.cos(theta_x)
+        sin_x = np.sin(theta_x)
+        cos_y = np.cos(theta_y)
+        sin_y = np.sin(theta_y)
+        cos_z = np.cos(theta_z)
+        sin_z = np.sin(theta_z)
+
+        R_x = np.array([[1, 0, 0],
+                        [0, cos_x, -sin_x],
+                        [0, sin_x, cos_x]])
+
+        R_y = np.array([[cos_y, 0, sin_y],
+                        [0, 1, 0],
+                        [-sin_y, 0, cos_y]])
+
+        R_z = np.array([[cos_z, -sin_z, 0],
+                        [sin_z, cos_z, 0],
+                        [0, 0, 1]])
+
+        R = np.dot(np.dot(R_z, R_y), R_x)
+
+        return R
+
+    def rotation_matrix_to_quaternion(self, m):
+        """
+        Convert a 3x3 rotation matrix to a quaternion.
+
+        Parameters:
+        - m: 3x3 NumPy array representing the rotation matrix.
+
+        Returns:
+        - q: 1x4 NumPy array representing the quaternion (x, y, z, w).
+        """
+        q = np.empty((4,), dtype=np.float64)
+        trace = m[0, 0] + m[1, 1] + m[2, 2]
+        if trace > 0:
+            s = 0.5 / np.sqrt(trace + 1.0)
+            q[3] = 0.25 / s
+            q[0] = (m[2, 1] - m[1, 2]) * s
+            q[1] = (m[0, 2] - m[2, 0]) * s
+            q[2] = (m[1, 0] - m[0, 1]) * s
+        else:
+            if m[0, 0] > m[1, 1] and m[0, 0] > m[2, 2]:
+                s = 2.0 * np.sqrt(1.0 + m[0, 0] - m[1, 1] - m[2, 2])
+                q[3] = (m[2, 1] - m[1, 2]) / s
+                q[0] = 0.25 * s
+                q[1] = (m[0, 1] + m[1, 0]) / s
+                q[2] = (m[0, 2] + m[2, 0]) / s
+            elif m[1, 1] > m[2, 2]:
+                s = 2.0 * np.sqrt(1.0 + m[1, 1] - m[0, 0] - m[2, 2])
+                q[3] = (m[0, 2] - m[2, 0]) / s
+                q[0] = (m[0, 1] + m[1, 0]) / s
+                q[1] = 0.25 * s
+                q[2] = (m[1, 2] + m[2, 1]) / s
+            else:
+                s = 2.0 * np.sqrt(1.0 + m[2, 2] - m[0, 0] - m[1, 1])
+                q[3] = (m[1, 0] - m[0, 1]) / s
+                q[0] = (m[0, 2] + m[2, 0]) / s
+                q[1] = (m[1, 2] + m[2, 1]) / s
+                q[2] = 0.25 * s
+        return q
+    
+    def quaternion_to_rotation_matrix(self, q):
+        # Ensure the quaternion is normalized
+        q = q / np.linalg.norm(q)
+
+        # Extract quaternion components
+        x, y, z, w = q
+
+        # Compute the rotation matrix elements
+        xx = x * x
+        xy = x * y
+        xz = x * z
+        xw = x * w
+
+        yy = y * y
+        yz = y * z
+        yw = y * w
+
+        zz = z * z
+        zw = z * w
+
+        # Build the rotation matrix
+        rotation_matrix = np.array([
+            [1 - 2 * (yy + zz), 2 * (xy - zw), 2 * (xz + yw)],
+            [2 * (xy + zw), 1 - 2 * (xx + zz), 2 * (yz - xw)],
+            [2 * (xz - yw), 2 * (yz + xw), 1 - 2 * (xx + yy)]
+        ])
+
+        return rotation_matrix
+
+def main():
+    position_publisher = PositionPublisher()
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
